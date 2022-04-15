be7701380f3fded4611f18d5c71193aa91b8dc91
parsa <parsamesg@gmail.com>
2022-04-15 23:19:12 +0100

    update keymap properly with symbols and num on one layer

--- keyboards/cantor/keymaps/pzrsa/keymap.c
+++ keyboards/cantor/keymaps/pzrsa/keymap.c
@@ -5,7 +5,7 @@
 
 // Tap Dance keycodes
 enum tap_dance_codes {
- DANCE_15,
+ CMD_SPC,
 };
 
 typedef struct {
@@ -37,44 +37,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LOPT,  KC_Z,   KC_X,    KC_C,      KC_V,    KC_B,                         KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LCMD, TD(DANCE_15),  MO(1),         MO(2),KC_ENT,KC_RSFT
+                                         KC_LCMD, TD(CMD_SPC), MO(1),     MO(2), KC_ENT, KC_RSFT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-  // num layer
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_TAB, XXXXXXX, XXXXXXX, KC_TILD, KC_MINS, KC_LBRC,                      KC_RBRC, KC_EQL, KC_BSLS,  XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LOPT, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
+      KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN,                      KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCMD,KC_SPC,_______,   _______, KC_ENT,KC_RSFT
+                                          XXXXXXX, XXXXXXX, _______,     XXXXXXX, KC_RSFT,KC_RCMD
                                       //`--------------------------'  `--------------------------'
   ),
 
-  // sym layer
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC, KC_EXLM,  KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,                      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LOPT, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LCMD,KC_SPC,_______,             _______,KC_ENT,KC_RSFT
+                                          KC_LCMD, KC_LSFT, XXXXXXX,     _______,XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 };
 
-void on_dance_15(qk_tap_dance_state_t *state, void *user_data);
-uint8_t dance_15_dance_step(qk_tap_dance_state_t *state);
-void dance_15_finished(qk_tap_dance_state_t *state, void *user_data);
-void dance_15_reset(qk_tap_dance_state_t *state, void *user_data);
+void on_cmd_spc(qk_tap_dance_state_t *state, void *user_data);
+uint8_t cmd_spc_dance_step(qk_tap_dance_state_t *state);
+void cmd_spc_finished(qk_tap_dance_state_t *state, void *user_data);
+void cmd_spc_reset(qk_tap_dance_state_t *state, void *user_data);
 
-void on_dance_15(qk_tap_dance_state_t *state, void *user_data) {
+void on_cmd_spc(qk_tap_dance_state_t *state, void *user_data) {
     if(state->count == 3) {
         tap_code16(KC_SPACE);
         tap_code16(KC_SPACE);
@@ -85,7 +83,7 @@ void on_dance_15(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-uint8_t dance_15_dance_step(qk_tap_dance_state_t *state) {
+uint8_t cmd_spc_dance_step(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
         if (state->interrupted || !state->pressed) return SINGLE_TAP;
         else return SINGLE_HOLD;
@@ -96,8 +94,8 @@ uint8_t dance_15_dance_step(qk_tap_dance_state_t *state) {
     }
     return MORE_TAPS;
 }
-void dance_15_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dance_state.step = dance_15_dance_step(state);
+void cmd_spc_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state.step = cmd_spc_dance_step(state);
     switch (dance_state.step) {
         case SINGLE_TAP: register_code16(KC_SPACE); break;
         case SINGLE_HOLD: register_code16(LGUI(KC_SPACE)); break;
@@ -105,8 +103,7 @@ void dance_15_finished(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void dance_15_reset(qk_tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
+void cmd_spc_reset(qk_tap_dance_state_t *state, void *user_data) {
     switch (dance_state.step) {
         case SINGLE_TAP: unregister_code16(KC_SPACE); break;
         case SINGLE_HOLD: unregister_code16(LGUI(KC_SPACE)); break;
@@ -116,5 +113,5 @@ void dance_15_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [DANCE_15] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_15, dance_15_finished, dance_15_reset),
+    [CMD_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(on_cmd_spc, cmd_spc_finished, cmd_spc_reset),
 };
