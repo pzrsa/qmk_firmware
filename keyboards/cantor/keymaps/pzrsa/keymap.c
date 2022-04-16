1eab00335033d596fe06b48d5cbabc12c044b083
parsa <parsamesg@gmail.com>
2022-04-16 20:30:10 +0100

    update keymap with a bigger update

--- keyboards/cantor/keymaps/pzrsa/keymap.c
+++ keyboards/cantor/keymaps/pzrsa/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LOPT,  KC_Z,   KC_X,    KC_C,      KC_V,    KC_B,                         KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LCMD, TD(CMD_SPC), MO(1),     MO(2), KC_ENT, KC_RSFT
+                                         KC_LCMD, TD(CMD_SPC), MO(2),     MO(1), KC_ENT, KC_RSFT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -52,23 +52,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, KC_TILD, KC_MINS, KC_LBRC,                      KC_RBRC, KC_EQL, KC_BSLS,  XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_TAB, XXXXXXX, KC_GRV, KC_MINS, KC_LBRC, XXXXXXX,                        XXXXXXX, KC_RBRC, KC_EQL,  KC_BSLS, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN,                      KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
+      KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, XXXXXXX,                      XXXXXXX, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, _______,     XXXXXXX, KC_RCMD, KC_RSFT
+                                          KC_LSFT, KC_LCMD, XXXXXXX,     _______, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCMD, KC_LSFT, XXXXXXX,     _______,XXXXXXX, XXXXXXX
+                                          XXXXXXX, XXXXXXX, _______,     XXXXXXX,KC_RCMD, KC_RSFT
                                       //`--------------------------'  `--------------------------'
   ),
 };
