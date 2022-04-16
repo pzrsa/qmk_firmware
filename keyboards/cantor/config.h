207c7a206f56825f6bf94ae0d2403bdf8a53cf2c
parsa <parsamesg@gmail.com>
2022-04-16 19:21:14 +0100

    refactor tap dance code

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -29,12 +29,3 @@
 /* Top right key on right half */
 #define BOOTMAGIC_LITE_ROW_RIGHT    4
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 5
-
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
--- keyboards/cantor/keymaps/pzrsa/keymap.c
+++ keyboards/cantor/keymaps/pzrsa/keymap.c
@@ -4,28 +4,34 @@
 #include QMK_KEYBOARD_H
 
 // Tap Dance keycodes
-enum tap_dance_codes {
- CMD_SPC,
+enum td_keycodes {
+    CMD_SPC,
 };
 
-typedef struct {
-    bool is_press_action;
-    uint8_t step;
-} tap;
+// Define a type containing as many tapdance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
 
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
-};
+// Create a global instance of the tapdance state type
+static td_state_t td_state;
 
-static tap dance_state = {
-    .is_press_action = true,
-    .step = 0
-};
+// Declare your tapdance functions:
+
+// Function to determine the current tapdance state
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+void cmd_spc_finished(qk_tap_dance_state_t *state, void *user_data);
+void cmd_spc_reset(qk_tap_dance_state_t *state, void *user_data);
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base layer
@@ -50,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LOPT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN,                      KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, _______,     XXXXXXX, KC_RSFT,KC_RCMD
+                                          XXXXXXX, XXXXXXX, _______,     XXXXXXX, KC_RCMD, KC_RSFT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -67,51 +73,85 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-void on_cmd_spc(qk_tap_dance_state_t *state, void *user_data);
-uint8_t cmd_spc_dance_step(qk_tap_dance_state_t *state);
-void cmd_spc_finished(qk_tap_dance_state_t *state, void *user_data);
-void cmd_spc_reset(qk_tap_dance_state_t *state, void *user_data);
-
-void on_cmd_spc(qk_tap_dance_state_t *state, void *user_data) {
-    if(state->count == 3) {
-        tap_code16(KC_SPACE);
-        tap_code16(KC_SPACE);
-        tap_code16(KC_SPACE);
-    }
-    if(state->count > 3) {
-        tap_code16(KC_SPACE);
-    }
-}
-
-uint8_t cmd_spc_dance_step(qk_tap_dance_state_t *state) {
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return TD_SINGLE_HOLD;
     } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
     }
-    return MORE_TAPS;
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
 }
+
+
+// Handle the possible states for each tapdance keycode you define:
 void cmd_spc_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dance_state.step = cmd_spc_dance_step(state);
-    switch (dance_state.step) {
-        case SINGLE_TAP: register_code16(KC_SPACE); break;
-        case SINGLE_HOLD: register_code16(LGUI(KC_SPACE)); break;
-        case DOUBLE_HOLD: register_code16(KC_SPACE); break;
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP: register_code(KC_SPC); break;
+        case TD_SINGLE_HOLD: register_code16(LCMD(KC_SPC)); break;
+        case TD_DOUBLE_TAP: register_code(KC_SPC); break;
+        case TD_DOUBLE_HOLD: register_code(KC_SPC); break;
+        // Last case is for fast typing. Assuming your key is `f`:
+        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+        case TD_DOUBLE_SINGLE_TAP: tap_code(KC_SPC); register_code(KC_SPC);
+        default: break;
     }
 }
 
 void cmd_spc_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (dance_state.step) {
-        case SINGLE_TAP: unregister_code16(KC_SPACE); break;
-        case SINGLE_HOLD: unregister_code16(LGUI(KC_SPACE)); break;
-        case DOUBLE_HOLD: unregister_code16(KC_SPACE); break;
+    switch (td_state) {
+        case TD_SINGLE_TAP: unregister_code(KC_SPC); break;
+        case TD_SINGLE_HOLD: unregister_code16(LCMD(KC_SPC)); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_SPC); break;
+        case TD_DOUBLE_HOLD: unregister_code(KC_SPC);
+        case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_SPC);
+        default: break;
     }
-    dance_state.step = 0;
 }
 
+// Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [CMD_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(on_cmd_spc, cmd_spc_finished, cmd_spc_reset),
+    [CMD_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cmd_spc_finished, cmd_spc_reset)
 };
