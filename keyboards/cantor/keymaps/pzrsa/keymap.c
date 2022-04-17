1da2f643052ac36da9c79d4b737efeba2c8e6229
parsa <parsamesg@gmail.com>
2022-04-17 16:17:25 +0100

    update tab switching

--- keyboards/cantor/keymaps/pzrsa/keymap.c
+++ keyboards/cantor/keymaps/pzrsa/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // nav and media layer
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                    XXXXXXX, RCTL(RSFT(KC_TAB)), RCTL(KC_TAB), XXXXXXX, XXXXXXX,XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                    XXXXXXX, RSG(KC_LBRC), RSG(KC_RBRC), XXXXXXX, XXXXXXX,XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
